b564ad1411c3ff36f38eb381e63714af4e2745a0
Yeow Zi Qin <ziqinyeow@gmail.com>
2021-11-19 16:28:54 +0800

    Integrate Prisma Data Proxy

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import useAuth from "../firebase/auth/hook/auth";
 
 export default function Login() {
-  const { user, loginWithGoogle }: any = useAuth();
+  const { user, loginWithGoogle, logout }: any = useAuth();
   return (
     <div>
       <button onClick={loginWithGoogle}>Login</button>
       <div>{user?.email}</div>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 }
